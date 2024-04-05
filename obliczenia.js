34a19a4105c7e2b44c026c50aedb85121f3a3c2a
Olga Iereshchenko <ola.manukur@gmail.com>
2024-04-05 19:58:47 +0200

    js 3

--- obliczenia.js
+++ obliczenia.js
@@ -6,11 +6,12 @@ function oblicz(){
     let nadplata = document.getElementById("nadplata").value*1;
     let jezemes_plat = suma_kredyta * 0.007 * 5.6 / (5.6 - 1);
     let ostatok_dolga = suma_kredyta;
+
     for (let i = 1; i <= miesiecy; i++)
     {
-        procentnaja_czast = ostatok_dolga *0.007;
-        osnowna_zcast = jezemes_plat - procentnaja_czast;
-        ostatok_dolga = ostatok_dolga - osnowna_zcast - nadplata;
+        let procentnaja_czast = ostatok_dolga *0.007;
+        let osnowna_zcast = jezemes_plat - procentnaja_czast;
+        let ostatok_dolga = ostatok_dolga - osnowna_zcast - nadplata;
         document.getElementById("wypis").innerHTML= i + " " + procentnaja_czast + " " + osnowna_zcast + " " + ostatok_dolga;
     
 }
